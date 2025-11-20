de9aa759dfc05d91ba8b6d822d065977b4a14f8f
jorgegalvez <jo.galvezr@duocuc.cl>
2025-11-20 12:42:01 -0300

    model usuario

--- src/main/java/com/example/backend/model/Pedido.java
+++ src/main/java/com/example/backend/model/Pedido.java
@@ -1,5 +1,5 @@
 package com.example.backend.model;
 
-public class Pedidos {
+public class Pedido {
     
 }
--- src/main/java/com/example/backend/model/Usuario.java
+++ src/main/java/com/example/backend/model/Usuario.java
@@ -1,5 +1,47 @@
 package com.example.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@Entity
+@Table(name="usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+
+    @Id
+    @Column(nullable = false, length=10)
+    private String rut;
+
+    @Column(nullable = false, length=40)
+    private String nombre;
+
+    @Column(nullable = false, length=40)
+    private String apellido;
+
+    @Column(nullable = false, length=40)
+    private String correo;
+
+    @Column(nullable = false, length=40)
+    private String contrasena;
+
+    @Column(nullable = false, length=40)
+    private String direccion;
+    
+    @Column(nullable = false, length=40)
+    private String region;
+
+    @Column(nullable = false, length=40)
+    private String comuna;
+
+    @Column(nullable = false, length=40)
+    private String fecha_nac;
+
+    @Column(length=40)
+    private String cod_descuento;
+
     
 }
