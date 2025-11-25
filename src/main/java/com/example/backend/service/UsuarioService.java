42e01a6c30927aa2d5cd5d2ad22a4d4b17fe83d4
jorgegalvez <jo.galvezr@duocuc.cl>
2025-11-25 13:35:07 -0300

    usuario creado

--- src/main/java/com/example/backend/service/UsuarioService.java
+++ src/main/java/com/example/backend/service/UsuarioService.java
@@ -1,5 +1,21 @@
 package com.example.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+
+import com.example.backend.model.Usuario;
+import com.example.backend.repository.UsuarioRepository;
+
+@Service
 public class UsuarioService {
     
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario crearUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
 }
